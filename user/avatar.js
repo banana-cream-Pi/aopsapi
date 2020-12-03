f601b80a4ef883f18deb72fba5da43b38378b803
banana-cream-Pi <46378514+banana-cream-Pi@users.noreply.github.com>
2020-12-03 09:16:01 -0500

    Update avatar.js

--- user/avatar.js
+++ user/avatar.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+//const browser = require('aopsapi/browservar');
 
 async function avatar(user){
     const browser = await puppeteer.launch({
@@ -10,10 +11,13 @@ async function avatar(user){
     const url = `https://artofproblemsolving.com/community/user/${user}`;
     
     await page.goto(url).catch();
-    await page.waitForSelector('#community-all > div > div:nth-child(2) > div.cmty-user-profile-main.cmty-index-card > div.cmty-user-profile-data > div.cmty-user-profile-avatar.cmty-no-phone');
+    await page.waitForSelector('#community-all > div > div:nth-child(2) > div.cmty-user-profile-main.cmty-index-card > div.cmty-user-profile-data > div.cmty-user-profile-avatar.cmty-no-phone').catch(err=>{if(err){console.log('No such user');return;}});
     
     
-    const imgs = await page.$$eval('.cmty-user-profile-avatar img[src]', imgs => imgs.map(img => img.getAttribute('src')));
+    const imgs = await page.$$eval('.cmty-user-profile-avatar img[src]', imgs => imgs.map(img => img.getAttribute('src'))).catch(err=>{if(err){console.log('No such user');return;}});
+    if (imgs === null){
+        return;
+    }
     
     
     browser.close().catch();
